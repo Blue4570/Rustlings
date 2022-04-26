7a841c3e2e96a2b6b345caa461585ac4aec8ef13
Aaron Butcher <butcheraaron@icloud.com>
2022-04-25 21:29:59 -0500

    Adding iterators 4 solution

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,7 +1,5 @@
 // iterators4.rs
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -12,6 +10,32 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    //Using std::iter:Iterator::fold
+    //Created a range slice using (1..=num)
+    //fold takes an initial value for v, and a closure with type parameters
+    //The first param is an accumulator which I have named acc, and 1 gets passed to value
+    //on the first iteration
+    //Refer to the std lib documentation for more details: https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.fold
+    //(1..=num).fold(1, |acc, v| acc * v)
+
+    //Another option would be to use reduce: std::iter:Iterator::reduce
+    //Reduce is the similar to fold(), but takes the first element as the initial value
+    //Refer to the std lib documentation for more details: https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.reduce
+    (1..=num).reduce(|sum, v| sum * v).unwrap()
+
+    //Or you could use recursion as the traditional approach
+    //Obviously using functional programming style requires less code
+    //Rust compiler also provides optimizations when using the functional style
+    //that are proven to perform slightly better than using the traditional looping or recursion technique
+
+    // if num <= 1 {
+    //     num
+    // } else {
+    //     num * factorial(num -1)
+    // }
+
+
 }
 
 #[cfg(test)]
